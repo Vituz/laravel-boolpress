8d66e088203fb36203b63e11a66c5f3c981506de
Vituz <vtbattaglia@gmail.com>
2021-07-27 16:28:19 +0200

    Create category_id on posts table

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view();
+    }
 }
--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
+
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +15,14 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ['Nature', 'Tech', 'Food', 'Animal'];
+
+        foreach ($categories as $category) {
+            $cat = new Category();
+
+            $cat->name = $category;
+            $cat->slug = Str::slug($cat->name);
+            $cat->save();
+        }
     }
 }
